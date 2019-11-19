73810320696ed38ea9292b8960c646009e2fa539
williams.chinwa <chinwawilliams@gmail.com>
2019-11-19 16:22:10 +0100

    first commit

---
+++ config.csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "b4eaaf21f865caa91bec2f7273b085efd9ea1fe5";
---
+++ test/templates_c/v7/049cfc0244e9c4e31ee3af803a2eab07925b3d76.file.InstallPreProcess.tpl.php
@@ -0,0 +1,41 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-11-19 15:03:54
+         compiled from "/Applications/MAMP/htdocs/vtigercrm/includes/runtime/../../layouts/v7/modules/Install/InstallPreProcess.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:9688699205dd4045ae0fdd0-24270390%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '049cfc0244e9c4e31ee3af803a2eab07925b3d76' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/vtigercrm/includes/runtime/../../layouts/v7/modules/Install/InstallPreProcess.tpl',
+      1 => 1572870387,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '9688699205dd4045ae0fdd0-24270390',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5dd4045ae1aff',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5dd4045ae1aff')) {function content_5dd4045ae1aff($_smarty_tpl) {?>
+
+<input type="hidden" id="module" value="Install" />
+<input type="hidden" id="view" value="Index" />
+<div class="container-fluid page-container">
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="logo">
+				<img src="<?php echo vimage_path('logo.png');?>
+"/>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="head pull-right">
+				<h3><?php echo vtranslate('LBL_INSTALLATION_WIZARD','Install');?>
+</h3>
+			</div>
+		</div>
+	</div>
+<?php }} ?>
